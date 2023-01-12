394650caceb7d80345701503254f17b7cadaecb9
denyslyman <82877658+denyslyman@users.noreply.github.com>
2023-01-12 16:01:20 +0200

    Second commit: start command + main menu + default command

--- src/main/java/io/telegrambot/config/TelegramBot.java
+++ src/main/java/io/telegrambot/config/TelegramBot.java
@@ -1,10 +1,10 @@
 package io.telegrambot.config;
 
+import io.telegrambot.keyboards.KeyboardsBuilder;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -15,11 +15,17 @@ import java.util.List;
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    private String start = "/start";
+
+    final KeyboardsBuilder keyboardsBuilder;
+
     final BotConfig config;
 
-    public TelegramBot(BotConfig config) {
+    public TelegramBot(KeyboardsBuilder keyboardsBuilder, BotConfig config) {
+        this.keyboardsBuilder = keyboardsBuilder;
         this.config = config;
     }
+
 
     @Override
     public String getBotUsername() {
@@ -33,58 +39,46 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+
         if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage sendMessage = new SendMessage();
             String messageText = update.getMessage().getText();
+
             Long chatId = update.getMessage().getChatId();
 
-            switch (messageText) {
-                case "/start":
-                    startMessage(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
-                    sendMessage(chatId, "Sorry, I'm just a bot \uD83E\uDD16 and sometimes" +
-                            "I don't understand what you write to me \uD83D\uDE1E\n" +
-                            "\n" +
-                            "To display the keyboard with answer options, please press the button \uD83D\uDC47");
-
+            if (messageText.equals("/start")) {
+                startMessage(chatId, update.getMessage().getChat().getFirstName());
+            } else {
+                wrongMessageText(chatId, "Sorry, I'm just a bot \uD83E\uDD16 and sometimes" +
+                        "I don't understand what you write to me \uD83D\uDE1E\n" +
+                        "\n" +
+                        "To display the keyboard with answer options, please press the button \uD83D\uDC47");
             }
         }
+
     }
 
     private void startMessage(Long chatId, String name) {
 
+        SendMessage sendMessage = new SendMessage();
         String startAnswer = "Hi, " + name + ", nice to meet you";
-        sendMessage(chatId, startAnswer);
+        sendMessage.setText(startAnswer);
+        sendMessage.setChatId(String.valueOf(chatId));
+        keyboardsBuilder.startMenuBuilder(sendMessage);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
 
 
     }
-
-    private void sendMessage(Long chatId, String textToSend) {
+    private void wrongMessageText(Long chatId,String textToSend){
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setText(textToSend);
         sendMessage.setChatId(String.valueOf(chatId));
-
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        keyboardMarkup.setResizeKeyboard(true);
-
-        List<KeyboardRow> keyboardRows = new ArrayList<>();
-
-        KeyboardRow row = new KeyboardRow();
-
-        row.add("Training\uD83C\uDFCB️\u200D♂️");
-        row.add("Feedback✍️");
-
-        keyboardRows.add(row);
-
-        row = new KeyboardRow();
-        row.add("Information❗️");
-        row.add("Chat of participants\uD83D\uDCAC");
-
-        keyboardRows.add(row);
-
-        keyboardMarkup.setKeyboard(keyboardRows);
-
-        sendMessage.setReplyMarkup(keyboardMarkup);
+        sendMessage.setText(textToSend);
 
         try {
             execute(sendMessage);
